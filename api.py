54976f936a3d21e9c22975711dcec26acb17bcad
Alboneh <ibrahim.1208@students.amikom.ac.id>
2022-05-31 19:40:52 +0700

    add post from daniel

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import os
 from datetime import timedelta
+from typing import Union
 from flask import Flask,jsonify,request,make_response
 from util import Preprocessing
 from flask_jwt_extended import JWTManager,create_access_token,jwt_required,get_jwt_identity
@@ -7,10 +8,14 @@ from database import init,getalluser,registerdb,logindb
 from flask_cors import CORS
 
 
-preprocess = Preprocessing("model.h5")
+#init flask and sql
 app = Flask(__name__)
 CORS(app)
 mysql = init(app)
+
+#load tensorflow model
+preprocess = Preprocessing("model.h5")
+prediction_results = preprocess.predict()
 
 #JWT
 app.config["JWT_SECRET_KEY"] = "capstone-secret-key" 
@@ -23,25 +28,6 @@ def index():
     #return jsonify(logged_in_as=current_user), 200
     return "Hello World"
 
-@app.route('/register', methods=['POST'])
-def register():
-    username = request.form['username']
-    email = request.form['email']
-    pwd = request.form['password']
-    return registerdb(mysql,username,email, pwd)
- 
-@app.route('/users',methods=['GET'])
-def userlist():
-    user = getalluser(mysql)
-    return jsonify(user)
-
-    
-@app.route('/predict', methods=['GET'])
-@jwt_required()
-def predict():
-  data = {'success': 'true','data': preprocess.predict()}                                                                                                                                                                                                                                                 
-  return jsonify(data)
-
 @app.route('/login', methods=['POST'])
 def login():
     email = request.form['email']
@@ -52,6 +38,53 @@ def login():
         data = {"message": "Login Successful" , "user": user, "access_token" : access_token}
         return jsonify(data),200
     return jsonify({"msg": "username atau password salah"}), 401
+
+@app.route('/predict', methods=['GET'])
+@jwt_required()
+def predict():
+  data = {'success': 'true','data': prediction_results}                                                                                                                                                                                                                                                 
+  return jsonify(data)
+
+@app.route("/predict/<product_name>", methods=['GET'])
+def predictByProductName(product_name, days: Union[int, None] = None):
+    try:
+        data = next(product for product in prediction_results if product["product_name"] == product_name)
+    except:
+        return jsonify(status_code=404, content = {"message": f"Product '{product_name}' doesn\'t exist"})
+
+    if (days):
+        data["predictions"] = data["predictions"][:days]
+        return data
+    return data
+
+@app.route("/<product_name>", methods=['POST'])
+def input_product(product_name):
+    input_date = request.form['input_date']
+    sold = request.form['sold']
+    try:
+        data = next(product for product in prediction_results if product["product_name"] == product_name)
+    except:
+        return jsonify(status_code=404, content = {"message": f"Product '{product_name}' doesn\'t exist"})
+    
+    prediction = next(item for item in data["predictions"] if item["date"] == input_date)
+    prediction["real"] = sold
+    return data
+
+
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['name']
+    email = request.form['email']
+    pwd = request.form['password']
+    return registerdb(mysql,username,email, pwd)
+ 
+@app.route('/users',methods=['GET'])
+def userlist():
+    user = getalluser(mysql)
+    return jsonify(user)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)))
---
+++ coba.py
@@ -0,0 +1,6 @@
+run = float(input("Sample "))
+
+desk = type(run)
+
+print(desk)
+print(run)
